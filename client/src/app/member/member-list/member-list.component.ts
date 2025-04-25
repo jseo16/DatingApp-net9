1dcb45c45db5c5f8cb102b43c7db2fc8f103a019
jseo16 <joseo46@hotmail.com>
2025-04-25 15:54:01 +1000

    feat: create MemberCard component and update member list display

--- client/src/app/member/member-list/member-list.component.ts
+++ client/src/app/member/member-list/member-list.component.ts
@@ -1,19 +1,18 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { distinctUntilChanged } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Member } from '../../_models/member';
+import { MemberCardComponent } from '../../members/member-card/member-card.component';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [],
+  imports: [MemberCardComponent],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.scss',
 })
 export class MemberListComponent implements OnInit {
-  private http = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
   private membersService = inject(MembersService);
   members: Member[] = [];
