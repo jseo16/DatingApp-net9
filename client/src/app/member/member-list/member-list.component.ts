5d06daf094d2236496df2a82cb2d6969decf4e34
jseo16 <joseo46@hotmail.com>
2025-04-25 15:43:34 +1000

    feat: implement member list display and data loading

--- client/src/app/member/member-list/member-list.component.ts
+++ client/src/app/member/member-list/member-list.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { distinctUntilChanged } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Member } from '../../_models/member';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
   imports: [],
   templateUrl: './member-list.component.html',
-  styleUrl: './member-list.component.scss'
+  styleUrl: './member-list.component.scss',
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit {
+  private http = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+  private membersService = inject(MembersService);
+  members: Member[] = [];
 
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.membersService
+      .getMembers()
+      .pipe(distinctUntilChanged(), takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (members) => (this.members = members),
+      });
+  }
 }
